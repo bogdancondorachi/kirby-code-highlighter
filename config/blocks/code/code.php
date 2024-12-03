43bdf75207d87885824a542474e0c0ff62c6c7b8
Bogdan Condorachi <bogdan.condorachi@gmail.com>
2024-12-03 21:35:49 +0200

    feat: enable dual themes

--- config/blocks/code/code.php
+++ config/blocks/code/code.php
@@ -6,9 +6,15 @@ $highlighter = new \Phiki\Phiki();
 $code   = $block->code()->fromBase64();
 $lang   = $block->language()->or('text');
 $theme  = $block->theme()->or(option('bogdancondorachi.code-highlighter.theme'));
+$themes = option('bogdancondorachi.code-highlighter.themes');
 $gutter = option('bogdancondorachi.code-highlighter.gutter');
 
-$highlightedCode = $highlighter->codeToHtml($code, $lang, $theme, $gutter);
+if (!is_null($themes) && (!is_array($themes) || array_values($themes) === $themes)) {
+  // Throw an exception if $themes is not an associative array
+  throw new InvalidArgumentException('The themes option must be an associative array');
+}
+
+$highlightedCode = $highlighter->codeToHtml($code, $lang, $themes ?? $theme, $gutter);
 
 ?>
 
