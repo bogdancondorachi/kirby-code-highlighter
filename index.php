c563a86052746b879d8ff5ee4a3193fe8dda9082
Bogdan Condorachi <bogdan.condorachi@gmail.com>
2024-12-19 04:37:02 +0200

    refactor: clean up index.php

--- index.php
+++ index.php
@@ -2,72 +2,13 @@
 
 @include_once __DIR__ . '/vendor/autoload.php';
 
-use Phiki\Theme\Theme;
-use Kirby\Toolkit\Str;
+use Kirby\Cms\App;
 
-Kirby::plugin('bogdancondorachi/code-highlighter', [
-  'options' => [
-    'language' => 'text',
-    'theme' => 'github-dark-dimmed',
-    'gutter' => false,
-    'block' => [
-      'languages' => [
-        'bash' => 'Bash',
-        'blade' => 'Blade',
-        'c' => 'C',
-        'coffee' => 'CoffeeScript',
-        'cpp' => 'C++',
-        'css' => 'CSS',
-        'glsl' => 'GLSL',
-        'graphql' => 'GraphQL',
-        'handlebars' => 'Handlebars',
-        'html' => 'HTML',
-        'http' => 'HTTP',
-        'java' => 'Java',
-        'js' => 'JavaScript',
-        'json' => 'JSON',
-        'jsx' => 'JSX',
-        'less' => 'Less',
-        'markdown' => 'Markdown',
-        'php' => 'PHP',
-        'text' => 'Plain Text',
-        'python' => 'Python',
-        'regexp' => 'RegExp',
-        'sass' => 'Sass',
-        'scss' => 'SCSS',
-        'shell' => 'Shell',
-        'sql' => 'SQL',
-        'stylus' => 'Stylus',
-        'svelte' => 'Svelte',
-        'tsx' => 'TSX',
-        'typescript' => 'TypeScript',
-        'vue' => 'Vue',
-        'xml' => 'XML',
-        'yaml' => 'YAML'
-      ],
-      'themes' => array_combine(
-        array_map(fn(Theme $theme) => $theme->value, Theme::cases()),
-        array_map(fn(Theme $theme) => Str::ucwords(Str::replace($theme->value, '-', ' ')), Theme::cases())
-      )
-    ]
-  ],
+App::plugin('bogdancondorachi/code-highlighter', [
 
-  'api' => [
-    'routes' => [
-      [
-        'pattern' => 'code-highlighter',
-        'action'  => function () {
-          $options = [
-            'lang'   => option('bogdancondorachi.code-highlighter.language'),
-            'theme'  => option('bogdancondorachi.code-highlighter.theme'),
-            'gutter' => option('bogdancondorachi.code-highlighter.gutter'),
-          ];
+  'options' => require __DIR__ . '/config/options.php',
 
-          return $options;
-        }
-      ]
-    ]
-  ],
+  'api' => require __DIR__ . '/config/api.php',
 
   'tags' => require __DIR__ . '/config/tags.php',
   'hooks' => require __DIR__ . '/config/hooks.php',
@@ -79,5 +20,5 @@ Kirby::plugin('bogdancondorachi/code-highlighter', [
   
   'snippets' => [
     'blocks/code' => __DIR__ . '/config/blocks/code/code.php'
-  ]
+  ],
 ]);
